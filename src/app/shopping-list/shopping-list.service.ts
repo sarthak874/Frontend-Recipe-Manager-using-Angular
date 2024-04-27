f8b6dc04e6db08d82d1d5aa2d33cb976e75e5be2
sarthakdarji <sarthak.darji@auberginesolutions.com>
2024-04-27 12:05:53 +0530

    used subject observable

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,12 +1,13 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListService {
 
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
   private ingredients : Ingredient[] = [
     new Ingredient('Apple',10),
     new Ingredient('Tomato',5)
@@ -20,7 +21,7 @@ export class ShoppingListService {
 
   addIngredient(ingredient : Ingredient){
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
   addIngredients(ingredients : Ingredient[]){
@@ -32,7 +33,7 @@ export class ShoppingListService {
     );
 
     this.ingredients.push(...uniqueIngredients);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
     // this.ingredients.push(...ingredients);
     // this.ingredientsChanged.emit(ingredients);
   }
